2fd5262975f7c0ff7ba25d0da28b7cf33a9fb702
shubham-mathur <mathurshubham@gmail.com>
2024-06-23 20:56:23 +0530

    added jwt authentication to signup and signin routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
     Bindings:{
       DATABASE_URL: string;
+      JWT_SECRET: string
     }
    }>()
 
@@ -20,26 +22,60 @@ app.post('/api/v1/user/signup',async (c)=> {
   }).$extends(withAccelerate())
 
   try{
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data:{
         username: body.username,
         password: body.password,
         name: body.name
       }
     })
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET);
 
-    return c.text("Hello from Signup!")
+    return c.text(jwt)
   }catch(e){
     console.log(e)
     c.status(411)
     return c.text('Invalid');
   }
+
+
+
 })
 
 
 
-app.post('/api/v1/user/signin',(c)=> {
-  return c.text("Hello from Signin!")
+app.post('/api/v1/user/signin', async (c)=> {
+  const body = await c.req.json();
+  //add zod
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  try{
+    const user = await prisma.user.findFirst({
+      where:{
+        username: body.username,
+        password: body.password,
+      }
+    })
+    if(!user){
+        c.status(403);
+        return c.json({
+          message: "Invalid, username or password doesn't match."
+        })
+      }
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET);
+
+    return c.text(jwt)
+  }catch(e){
+    console.log(e)
+    c.status(411)
+    return c.text('Invalid');
+  }
 })
 
 app.post('/api/v1/blog',(c)=> {

8c0f1216257121342000502fe119c7c0af155d16
shubham-mathur <mathurshubham@gmail.com>
2024-06-23 21:26:15 +0530

    added blog routes

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,5 +1,9 @@
 
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 import { Hono } from 'hono';
+
+
 
 export const blogRouter = new Hono<{
     Bindings:{    
@@ -8,20 +12,95 @@ export const blogRouter = new Hono<{
     }
 }>();
 
+blogRouter.use("/*", (c,next)=>{
+    next();
+});
 
 
-blogRouter.post('/',(c)=> {
-    return c.text("Hello from blog!")
+
+blogRouter.post('/', async (c)=> {
+    const body = await c.req.json();
+    //add zod
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const blog = await prisma.blog.create({
+        data: {
+            title: body.title,
+            content: body.content,
+            authorId: 1
+        }
+    })
+
+    return c.json({
+        id: blog.id
+    })
   })
   
-blogRouter.put('/',(c)=> {
-return c.text("Hello from blog! put ")
-})
+blogRouter.put('/', async (c)=> {
+    const body = await c.req.json();
+    //add zod
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
 
-blogRouter.get('/',(c)=>{
-return c.text("hello from blog id")
-})
+    const blog = await prisma.blog.update({
+        where:{
+            id: body.id
+        },
+        data: {
+            title: body.title,
+            content: body.content,
+        }
+    })
 
-blogRouter.get("/bulk",(c)=>{
-return c.text('hello from blog bulk')
+    return c.json({
+        id: blog.id
+    })
+  })
+
+blogRouter.get('/',async (c)=>{
+    const body = await c.req.json();
+    //add zod
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    try{
+        const blog = await prisma.blog.findFirst({
+            where:{
+                id: body.id
+            }
+        })
+    
+        return c.json({
+            blog
+        });
+    }catch(e){
+        c.status(411); //find 400 series for error fetching.
+        return c.json({
+            message: "error while fetching blogpost",
+            error: e
+        });
+    }
+
+
+  })
+
+
+blogRouter.get("/bulk",async (c)=>{
+    const body = await c.req.json();
+    //add zod
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const blogs = await prisma.blog.findMany();
+
+    //learn about pagination and implement here;
+
+    return c.json({
+        blogs
+    });
 })

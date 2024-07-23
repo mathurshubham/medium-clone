d37091d08360f233212e08a84a87a631ce35b2f9
shubham-mathur <mathurshubham@gmail.com>
2024-07-24 01:55:58 +0530

    completed auth component

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Quote } from '../components/Quote'
+import Auth from '../components/Auth'
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div className='grid grid-cols-2'>
+      <div>
+        <Auth type="signin"></Auth>
+      </div>
+      <div className='invisible md:visible'>
+        <Quote/>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Quote } from '../components/Quote'
+import Auth from '../components/Auth'
 
 const Signup = () => {
   return (
     <div className='grid grid-cols-2'>
       <div>
-
+        <Auth type="signin"></Auth>
       </div>
       <div className='invisible md:visible'>
         <Quote/>

4d9bec58d8704f006985197077ce6a72f3cce061
shubham-mathur <mathurshubham@gmail.com>
2024-07-23 12:02:36 +0530

    created signup,signin,blog routes. Created Signup Quotes Component

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Quote } from '../components/Quote'
+
+const Signup = () => {
+  return (
+    <div className='grid grid-cols-2'>
+      <div>
+
+      </div>
+      <div className='invisible md:visible'>
+        <Quote/>
+      </div>
+    </div>
+  )
+}
+
+export default Signup

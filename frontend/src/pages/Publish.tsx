65f7090e1d01ff25d486b9d6d27642b0d81250a9
shubham-mathur <mathurshubham@gmail.com>
2024-08-01 01:01:02 +0530

    fixed type errors and removed unused imports

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Appbar } from '../components/Appbar'
 import PublishTextEditor from '../components/PublishTextEditor'
 import axios from 'axios';

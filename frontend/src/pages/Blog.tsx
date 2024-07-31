7961a1f721ee03233395df7ff883c9bf74ecb68a
shubham-mathur <mathurshubham@gmail.com>
2024-08-01 00:55:41 +0530

    mvp frontend

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,4 +1,6 @@
+import { Appbar } from "../components/Appbar";
 import { FullBlog } from "../components/FullBlog";
+import Spinner from "../components/Spinner";
 import { useBlog } from "../hooks"
 import { useParams } from "react-router-dom";
 
@@ -9,7 +11,12 @@ export const Blog = () => {
   });
   if (loading){
     return <div>
-      loading...
+      <Appbar/> 
+      <div className="h-screen flex flex-col justify-center">
+        <div className="flex justify-center">
+          <Spinner/>  
+        </div>      
+      </div>
     </div>
   }
   return <div>
